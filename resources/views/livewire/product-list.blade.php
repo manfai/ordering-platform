e22362ddeffa653405b635f802ab68397f9b31aa
Man Wing Fai <manfai106@gmail.com>
2021-08-30 13:19:16 +0800

    added new logo

--- resources/views/livewire/product-list.blade.php
+++ resources/views/livewire/product-list.blade.php
@@ -15,7 +15,7 @@
                     <div wire:loading.remove wire:loading.target="changeBrand" class="col-span-6 md:col-span-4 lg:col-span-4 xl:col-span-3 md:flex pb-8 px-4 w-full">
                         <div class="card bordered shadow-lg w-full rounded-box bg-base-200">
                             <figure class="px-4 pt-4">
-                                <img src="{{$product->image_file? $product->image_file : 'https://image.freepik.com/free-psd/delivery-food-brown-box-mockup_181945-514.jpg'}}" class="h-40 object-cover object-center rounded-box bg-default-parttern bg-cover bg-center">
+                                <img src="{{$product->image_file? $product->image_file : ''}}" class="h-40 object-cover object-center rounded-box bg-default-parttern bg-cover bg-center">
                             </figure> 
                             <div class="card-body h-30 px-5 pt-4 pb-0">
                             <span class="menu-title text-opacity-50 text-xs text-gray-800">
@@ -36,7 +36,11 @@
                                 <h3 class="text-md font-bold">
                                     ${{$product->price}}
                                 </h3>
+                                @auth
                                 <button wire:click="addToCart({{$product->id}},'{{$menu_date}}')" class="btn btn-primary btn-sm text-sm m-0 rounded-lg">{{__('Add To Cart')}}</button>
+                                @else
+                                <a href="{{route('login')}}" class="btn btn-primary btn-sm text-sm m-0 rounded-lg">{{__('Add To Cart')}}</a>
+                                @endauth
                             </div>
                         </div> 
                         </div> 
