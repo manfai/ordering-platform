02aae1c7b19d18f20390c8d5f7c0af242f91dbc7
Man Wing Fai <manfai106@gmail.com>
2021-09-02 16:27:54 +0800

    no image

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -11,7 +11,7 @@
 <section class="flex flex-col md:flex-row h-screen items-center">
 
     <div class="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-      <img src="https://images.squarespace-cdn.com/content/v1/58461f2b29687f12c6fce065/1560934347168-70X3E7P1UHPKREH7EM3S/ke17ZwdGBToddI8pDm48kAt72yGFwHZjoxtmj75n0VMUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2dvp1wM0jvciobd5mvjBb-PkjbbxSYDSdt-BIyUswy_5eG6v6ULRah83RgHXAWD5lbQ/soul-food-thai-hong-kong.jpg?format=2500w" alt="" class="w-full h-full object-cover">
+      <img src="" alt="" class="w-full h-full object-cover">
     </div>
   
     <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
