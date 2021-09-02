80ff4ea5897d2f887e197f0eb7cfa3b4f9d3aa7f
Man Wing Fai <manfai106@gmail.com>
2021-09-02 16:30:32 +0800

    testing

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -12,6 +12,7 @@
 
     <div class="bg-primary-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
       <img src="https://ecbento.com/2.0/wp-content/uploads/2020/08/EC_bg-1.jpg" alt="" class="w-full h-full object-cover">
+      <div class="hero-overlay bg-opacity-60"></div>
     </div>
   
     <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
