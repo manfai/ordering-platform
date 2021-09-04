4cae8bf0202116eb9194a2150c605547c19b1f97
Man Wing Fai <manfai106@gmail.com>
2021-09-04 17:13:02 +0800

    added new year

--- resources/views/livewire/checkout-card.blade.php
+++ resources/views/livewire/checkout-card.blade.php
@@ -168,6 +168,18 @@
               <option value="2026">2026</option>
               <option value="2027">2027</option>
               <option value="2028">2028</option>
+              <option value="2029">2029</option>
+              <option value="2030">2030</option>
+              <option value="2031">2031</option>
+              <option value="2032">2032</option>
+              <option value="2033">2033</option>
+              <option value="2034">2034</option>
+              <option value="2035">2035</option>
+              <option value="2036">2036</option>
+              <option value="2037">2037</option>
+              <option value="2038">2038</option>
+              <option value="2039">2039</option>
+              <option value="2040">2040</option>
             </select>
             <input type="text" id="cvc" x-data="$('#cvc').inputmask('999');" wire:model="cvc" placeholder="cvc">
           </div>
