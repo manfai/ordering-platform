ed8834e18c2fbd6b9271dab8413e66f981f63a22
manfai <manfai106@gmail.com>
2021-09-10 14:47:21 +0800

    hotfix

--- resources/views/livewire/user/user-notify.blade.php
+++ resources/views/livewire/user/user-notify.blade.php
@@ -1,4 +1,4 @@
-<div class="stack animate-ping mb-8 w-full">
+<div class="stack animate-Pulse mb-8 w-full">
     <div class="shadow-md card bg-primary text-primary-content">
         <div class="card-body">
             <h2 class="card-title animate-pulse">Friendly Reminder</h2>
