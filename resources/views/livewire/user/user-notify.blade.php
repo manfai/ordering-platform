190655949bec8ce6392c6e14ba80026470177576
manfai <manfai106@gmail.com>
2021-09-10 14:45:49 +0800

    testing

--- resources/views/livewire/user/user-notify.blade.php
+++ resources/views/livewire/user/user-notify.blade.php
@@ -1,4 +1,4 @@
-<div class="stack mb-8 w-full">
+<div class="stack animate-pulse mb-8 w-full">
     <div class="shadow-md card bg-primary text-primary-content">
         <div class="card-body">
             <h2 class="card-title">Friendly Reminder</h2>
