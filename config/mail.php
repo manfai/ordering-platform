9e82c10d7d56675585470c36787981952e05c8a9
Man Wing Fai <manfai106@gmail.com>
2021-11-01 12:40:42 +0800

    testing

--- config/mail.php
+++ config/mail.php
@@ -37,8 +37,8 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'port' => 465,
+            'encryption' => 'tls',
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
