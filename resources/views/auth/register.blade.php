0f0ae972e8006abd980f9bd08c4e18d403c3b3b3
Man Wing Fai <manfai106@gmail.com>
2021-11-18 12:41:31 +0800

    testing

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -52,9 +52,8 @@
                         <div class="flex items-center">
                             <x-jet-checkbox name="terms" id="terms"/>
                             <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
+                                {!! __('I agree to the :terms_of_service', [
+                                        'terms_of_service' => '<a target="_blank" href="'.route('t&c').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms and Conditions').'</a>',
                                 ]) !!}
                             </div>
                         </div>
